13e9c0a93a4a4d3bbc9eb18916a6efd94b36aed7
Dahlia Graves <dahlialolagraves@gmail.com>
2022-05-04 14:48:05 -0700

    added in the through info for the product tags, need to test

--- models/index.js
+++ models/index.js
@@ -13,6 +13,17 @@ Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
